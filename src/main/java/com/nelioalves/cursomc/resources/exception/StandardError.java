4e21326e4d041c99d72763758193a9d3369c12f1
Calvin Mendes Cattaneo <calvin.cattaneo@gmail.com>
2018-06-28 10:56:00 -0300

    Padronizando o formato de exceções

--- src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/StandardError.java
@@ -3,37 +3,61 @@ package com.nelioalves.cursomc.resources.exception;
 import java.io.Serializable;
 
 public class StandardError implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Integer status;
-	private String msg;
-	private Long timeStamp;
-	public StandardError(Integer status, String msg, Long timeStamp) {
-		super();
-		this.status = status;
-		this.msg = msg;
-		this.timeStamp = timeStamp;
-	}
-	public Integer getStatus() {
-		return status;
-	}
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
-	public String getMsg() {
-		return msg;
-	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-	public Long getTimeStamp() {
-		return timeStamp;
-	}
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-		
+    private static final long serialVersionUID = 1L;
+
+    private Long timeStamp;
+    private Integer status;
+    private String error;
+    private String msg;
+    private String path;
+
+    public StandardError(Long timeStamp, Integer status, String error, String msg, String path) {
+        super();
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.msg = msg;
+        this.path = path;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
 }
--- src/main/java/com/nelioalves/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ValidationError.java
@@ -5,22 +5,20 @@ import java.util.List;
 
 public class ValidationError extends StandardError {
 
-	private static final long serialVersionUID = 1L;
-	
-	private List<FieldMessage> errors = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-	}
+    private static final long serialVersionUID = 1L;
 
-	public List<FieldMessage> getErrors() {
-		return errors;
-	}
+    private List<FieldMessage> errors = new ArrayList<>();
 
-	public void addError(String fieldName, String mensagem) {
-		errors.add(new FieldMessage(fieldName, mensagem));
-	}
-	
-	
+    public ValidationError(Long timeStamp, Integer status, String error, String msg, String path) {
+        super(timeStamp, status, error, msg, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String mensagem) {
+        errors.add(new FieldMessage(fieldName, mensagem));
+    }
 
 }
